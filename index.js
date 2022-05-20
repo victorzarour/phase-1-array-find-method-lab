c220a5381b4ce5236135f122d404ff42ea6f0e49
Victor <vx_zz@hotmail.com>
2022-05-20 07:16:52 -0400

    commit tree 2537c90ea1597807145cc8cf380cdad2a783831d

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(array){
+  let item = array.find(element => element.result === "W")
+  if (item){
+    return item.year
+  } else
+    return undefined
+}
